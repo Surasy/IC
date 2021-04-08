ac66429a5859561cdb1f93756c32aada5cd3f6f5
Dani <danpad01@ucm.es>
2021-04-08 18:58:41 +0200

    Practica 2 Empezada

---
+++ P1IC/src/module-info.java
@@ -0,0 +1,10 @@
+module Practica2IC {
+	exports gui;
+	exports main;
+	exports algoritmo;
+	exports casillas;
+	exports matriz;
+
+	requires java.desktop;
+	requires java.logging;
+}
---
+++ P2IC/src/main/main.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class main {
+
+	private final static String RUTAATRIBUTOS = "recursos/AtributosJuego.txt";
+	private final static String RUTADATOS = "recursos/Juego.txt";
+	
+	public static void main(String[] args) {
+		try {
+			ArrayList<String> atributos = leerAtributos(RUTAATRIBUTOS);
+			ArrayList<ArrayList<String>> datos = leerDatos(RUTADATOS);
+			
+//			for(int i = 0; i < datos.size(); i++) {
+//				for(int j = 0; j < datos.get(i).size(); j++) {
+//					System.out.print(datos.get(i).get(j) + " ");
+//					
+//				}
+//				System.out.print("\n");
+//			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	private static ArrayList<String> leerAtributos(String nombreArchivo) throws IOException {
+		BufferedReader csvReader = new BufferedReader(new FileReader(nombreArchivo));
+		String row;
+		ArrayList<String> data = new ArrayList<String>();
+		while ((row = csvReader.readLine()) != null) {
+			Collections.addAll(data, row.split(","));
+		}
+		csvReader.close();
+		return data;
+	}
+	
+	private static ArrayList<ArrayList<String>> leerDatos(String nombreArchivo) throws IOException {
+		BufferedReader csvReader = new BufferedReader(new FileReader(nombreArchivo));
+		String row;
+		ArrayList<String> rowData;
+		ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+		
+		while ((row = csvReader.readLine()) != null) {
+			rowData = new ArrayList<String>();
+			Collections.addAll(rowData, row.split(","));
+		    data.add(rowData);
+		}
+		
+		csvReader.close();
+		return data;
+	}
+}
